e6bcd8ee7c55e4def479c61fae330e616ad166ce
Greg James <gregvjames99@gmail.com>
2025-07-10 19:51:24 -0500

    Add HostBuilder Extensions

---
+++ ReQueue/Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,41 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using ReQueue.Interfaces;
+using StackExchange.Redis;
+
+namespace ReQueue.Extensions;
+
+public static class HostBuilderExtensions
+{
+    public static IHostBuilder AddReQueue(this IHostBuilder hostBuilder, Action<RedisOptions> configureOptions)
+    {
+        return hostBuilder.ConfigureServices((context, services) =>
+        {
+            services.AddSingleton<IConnectionHub>(new ConnectionHub(configureOptions));
+        });
+    }
+    
+    public static IHostBuilder AddReQueue(this IHostBuilder hostBuilder, IDatabase database)
+    {
+        return hostBuilder.ConfigureServices((context, services) =>
+        {
+            services.AddSingleton<IConnectionHub, ConnectionHub>(provider =>
+            {
+                var loggerFactory = provider.GetService<ILoggerFactory>() ?? null;
+                
+                return new ConnectionHub(database, loggerFactory);
+            });
+        });
+    }
+    
+    public static IHostBuilder AddReQueue(this IHostBuilder hostBuilder)
+    {
+        return hostBuilder.ConfigureServices((context, services) =>
+        {
+            services.Configure<RedisOptions>(context.Configuration.GetSection("RedisOptions"));
+            services.AddSingleton<IConnectionHub, ConnectionHub>();
+        });
+    }
+}
